2f1833ce6d0337ed12d86fa0ad736220c00bf998
LeviathanST <phamquochung170505@gmail.com>
2025-05-15 21:32:45 +0700

    setup `allocator` for debug, release mode

--- src/main.zig
+++ src/main.zig
@@ -1,18 +1,38 @@
 const std = @import("std");
 const httpz = @import("httpz");
+
 const router = @import("router.zig");
-const Logger = @import("logger.zig");
+const util = @import("util.zig");
 const Handler = @import("handler.zig");
 
+var arena = std.heap.ArenaAllocator.init(std.heap.page_allocator);
+var debug_allocator = std.heap.DebugAllocator(.{}).init;
+
 pub fn main() !void {
-    const logger = Logger.init(std.io.getStdOut().writer());
-    const port = 3000;
+    const logger = util.Logger.init(std.io.getStdOut().writer());
+    const allocator, const is_debug = comptime switch (@import("builtin").mode) {
+        .Debug, .ReleaseFast => .{ debug_allocator.allocator(), true },
+        else => .{ arena.allocator(), false },
+    };
+    defer if (is_debug) {
+        const leaked = debug_allocator.deinit();
+        if (leaked == .leak) {
+            std.log.debug("Memory leak is detected", .{});
+        }
+    } else {
+        arena.deinit();
+    };
+    const port_env = try std.process.getEnvVarOwned(allocator, "PORT");
+    defer allocator.free(port_env); // Free by debug allocator, do nothing in release mode
+    const port = try std.fmt.parseInt(u16, port_env, 10);
 
-    var arena = std.heap.ArenaAllocator.init(std.heap.page_allocator);
-    defer arena.deinit();
+    var handler: Handler = try .init(allocator, logger);
+    defer handler.deinit();
 
-    var handler: Handler = .{ .logger = logger };
-    var server = try httpz.Server(*Handler).init(arena.allocator(), .{ .port = port }, &handler);
+    var server = try httpz.Server(*Handler).init(allocator, .{
+        .address = "0.0.0.0",
+        .port = port,
+    }, &handler);
     defer {
         server.stop();
         server.deinit();
