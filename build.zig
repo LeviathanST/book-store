3cad10ffb7a599d1e4ce88949bcc920acaaa4ff6
LeviathanST <phamquochung170505@gmail.com>
2025-05-15 21:33:44 +0700

    add deps `pg`, `zenv`, `httpz`

--- build.zig
+++ build.zig
@@ -14,11 +14,15 @@ pub fn build(b: *std.Build) !void {
         .target = target,
         .optimize = optimize,
     });
-
+    const pg = b.dependency("pg", .{
+        .target = target,
+        .optimize = optimize,
+    });
     b.installArtifact(exe);
     const run_exe = b.addRunArtifact(exe);
     const run_step = b.step("run", "Run the application");
 
     exe.root_module.addImport("httpz", httpz.module("httpz"));
+    exe.root_module.addImport("pg", pg.module("pg"));
     run_step.dependOn(&run_exe.step);
 }
