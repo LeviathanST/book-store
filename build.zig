2fd0891efeab0fd3fd58b1ffaae5e11f80ff7d5f
LeviathanST <phamquochung170505@gmail.com>
2025-05-18 01:39:24 +0700

    add(build): zlint as a optional step

--- build.zig
+++ build.zig
@@ -10,6 +10,16 @@ pub fn build(b: *std.Build) !void {
         .target = target,
         .optimize = optimize,
     });
+
+    // STEPS
+    const zlint_step = b.step("zlint", "Run zlint if exists");
+    if (b.findProgram(&.{"zlint"}, &.{}) catch null) |zlint_path| {
+        const zlint_cmd = b.addSystemCommand(&.{zlint_path});
+        zlint_step.dependOn(&zlint_cmd.step);
+        b.getInstallStep().dependOn(zlint_step);
+    }
+
+    // DEPENDENCIES
     const httpz = b.dependency("httpz", .{
         .target = target,
         .optimize = optimize,
@@ -22,12 +32,9 @@ pub fn build(b: *std.Build) !void {
         .target = target,
         .optimize = optimize,
     });
-    b.installArtifact(exe);
-    const run_exe = b.addRunArtifact(exe);
-    const run_step = b.step("run", "Run the application");
-
     exe.root_module.addImport("httpz", httpz.module("httpz"));
     exe.root_module.addImport("pg", pg.module("pg"));
     exe.root_module.addImport("zenv", zenv.module("zenv"));
-    run_step.dependOn(&run_exe.step);
+
+    b.installArtifact(exe);
 }
