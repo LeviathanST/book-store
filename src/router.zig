10cf25c57d87d0534b44c246125e8f479573d864
LeviathanST <phamquochung170505@gmail.com>
2025-05-20 23:58:16 +0700

    add(route): register, login, verify

--- src/router.zig
+++ src/router.zig
@@ -1,6 +1,7 @@
 const httpz = @import("httpz");
-const Handler = @import("handler.zig");
 const api = @import("api.zig");
+
+const Handler = @import("Handler.zig");
 
 fn ping(_: *Handler, _: *httpz.Request, res: *httpz.Response) !void {
     try res.json(.{ .content = "Ponggggggggggg!" }, .{ .emit_null_optional_fields = false });
@@ -9,6 +10,16 @@ pub fn setup(comptime T: type, server: *httpz.Server(T)) !void {
     const router = try server.router(.{});
 
     router.get("/", ping, .{});
-    router.post("/register", api.auth.register, .{});
+    router.post("/register", api.registerFn, .{});
+    router.post("/login", api.loginFn, .{});
+
+    // TODO: This route just for token verification testing,
+    // need to remove later
+    // TODO: use middleware for each route need auth verification
+    router.post("/verify", api.verifyFn, .{});
     return;
 }
+
+pub const RouteData = struct {
+    parsed_type: type,
+};
