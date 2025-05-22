c25a53bdb52fc9e739f24374c52137a6705c7daa
LeviathanST <phamquochung170505@gmail.com>
2025-05-22 23:12:32 +0700

    invoke validate() and parse leaky in dto parser

--- src/util/dto_parser.zig
+++ src/util/dto_parser.zig
@@ -2,18 +2,19 @@ const std = @import("std");
 const httpz = @import("httpz");
 const response = @import("../response.zig");
 const Handler = @import("../Handler.zig");
+
 pub fn fromReq(comptime T: type, req: *httpz.Request, handler: *Handler) !T {
+    if (!@hasDecl(T, "validate")) {
+        @compileError(std.fmt.comptimePrint("Struct {s} not have validate()!", .{@typeName(T)}));
+    }
     const body = req.body() orelse return response.GeneralError.EmptyBodyContent;
-    const parsed_body = try std.json.parseFromSlice(
+    const data = try std.json.parseFromSliceLeaky(
         T,
         req.arena,
         body,
         .{ .ignore_unknown_fields = true },
     );
 
-    if (!@hasDecl(T, "validate")) {
-        @compileError(std.fmt.comptimePrint("Struct {s} not have validate()!", .{@typeName(T)}));
-    }
-    try parsed_body.value.validate(handler);
-    return parsed_body.value;
+    try data.validate(handler);
+    return data;
 }
