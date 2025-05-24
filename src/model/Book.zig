d268a99ed8397a531b27b85861fdbf158f7db15d
LeviathanST <phamquochung170505@gmail.com>
2025-05-24 21:31:58 +0700

    add relationship between books and categories

--- src/model/Book.zig
+++ src/model/Book.zig
@@ -1,6 +1,7 @@
 const std = @import("std");
 const util = @import("../util.zig");
 const Handler = @import("../Handler.zig");
+const api = @import("../api.zig");
 
 pub const Book = @This();
 pub const InsertError = error{DuplicatedISBN};
@@ -20,10 +21,12 @@ pub fn insert(
     description: []const u8,
     isbn: []const u8,
     author: []const u8,
+    category: []const u8,
 ) !void {
     const conn = try handler.pool.acquire();
     defer conn.release();
 
+    try conn.begin();
     _ = conn.exec(
         \\ INSERT INTO book (title, description, isbn, author)
         \\ VALUES ($1, $2, $3, $4)
@@ -34,8 +37,27 @@ pub fn insert(
                 return InsertError.DuplicatedISBN;
             }
         }
+        try conn.rollback();
         return err;
     };
+    const row_affected = conn.exec(
+        \\ INSERT INTO book_category(category_id, book_id)
+        \\ SELECT c.id, b.id FROM category c, book b
+        \\ WHERE c.name = $1 AND b.isbn = $2
+    , .{ category, isbn }) catch |err| {
+        if (conn.err) |pg_err| {
+            util.log.err("{s}", .{pg_err.message});
+            if (pg_err.isUnique()) {
+                return InsertError.DuplicatedISBN;
+            }
+        }
+        try conn.rollback();
+        return err;
+    };
+    if (row_affected == 0)
+        return api.category.FindCategoryError.CategoryNotFound;
+
+    try conn.commit();
 }
 
 pub fn findAll(h: *Handler, allocator: std.mem.Allocator) !std.ArrayList(Book) {
@@ -65,6 +87,35 @@ pub fn findAll(h: *Handler, allocator: std.mem.Allocator) !std.ArrayList(Book) {
     return list;
 }
 
+pub fn findByCategory(h: *Handler, allocator: std.mem.Allocator, category: []const u8) !std.ArrayList(Book) {
+    const conn = try h.pool.acquire();
+    defer conn.release();
+
+    var list = std.ArrayList(Book).init(allocator);
+    errdefer list.deinit();
+
+    const rs = conn.queryOpts(
+        \\ SELECT * FROM book b
+        \\ LEFT JOIN book_category bc ON bc.book_id = b.id
+        \\ LEFT JOIN category c ON c.id = bc.category_id
+        \\ WHERE c.name = $1
+    ,
+        .{category},
+        .{ .column_names = true },
+    ) catch |err| {
+        if (conn.err) |pg_err| {
+            util.log.err("{s}", .{pg_err.message});
+        }
+        return err;
+    };
+    defer rs.deinit();
+
+    while (try rs.next()) |row| {
+        const value = try row.to(Book, .{ .map = .name });
+        try list.append(value);
+    }
+    return list;
+}
 /// Find many matches with a title
 pub fn findByTitle(handler: *Handler, allocator: std.mem.Allocator, title: []const u8) !std.ArrayList(Book) {
     const conn = try handler.pool.acquire();
@@ -136,14 +187,16 @@ pub fn deleteByISBN(h: *Handler, isbn: []const u8) !void {
 pub fn updateByISBN(
     h: *Handler,
     isbn: []const u8,
-    new_isbn: ?[]const u8,
-    new_title: ?[]const u8,
-    new_description: ?[]const u8,
-    new_author: ?[]const u8,
+    new_isbn: []const u8,
+    new_title: []const u8,
+    new_description: []const u8,
+    new_author: []const u8,
+    new_category: []const u8,
 ) !void {
     const conn = try h.pool.acquire();
     defer conn.release();
 
+    try conn.begin();
     const rows_affected = conn.exec(
         \\ UPDATE book
         \\ SET (isbn, title, description, author)
@@ -161,14 +214,37 @@ pub fn updateByISBN(
         if (conn.err) |pg_err| {
             util.log.err("{s}", .{pg_err.message});
             if (pg_err.isUnique()) {
-                h.err = "New ISBN already exists";
                 return InsertError.DuplicatedISBN;
             }
         }
+        try conn.rollback();
         return err;
     };
 
     if (rows_affected == 0) {
+        try conn.rollback();
         return FindError.BookNotFound;
     }
+
+    _ = conn.exec(
+        \\ UPDATE book_category 
+        \\ SET category_id = (SELECT id FROM category WHERE name = $1)
+        \\ WHERE book_id = (SELECT id FROM book WHERE isbn = $2)
+    ,
+        .{
+            new_category,
+            new_isbn,
+        },
+    ) catch |err| {
+        if (conn.err) |pg_err| {
+            util.log.err("{s}", .{pg_err.message});
+            if (pg_err.isUnique()) {
+                return InsertError.DuplicatedISBN;
+            }
+        }
+        try conn.rollback();
+        return err;
+    };
+
+    try conn.commit();
 }
